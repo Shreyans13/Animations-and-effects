0cd9a74a2300a131b2f569a687dccfa95aa917f3
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-03-26 12:11:09 +0530

    Animation - 7 Ballons javascript

---
+++ Animation-7 Javascript Fun _ Append Element On Mousemove And Remove Element After Few Seconds/styles/js/master.js
@@ -0,0 +1,17 @@
+document.addEventListener("mousemove",function(e){
+	var ballons = document.querySelector('.ballons');
+	var bubbles = document.createElement('span');
+	var x = e.offsetX;
+	var y = e.offsetY;
+	bubbles.style.left = x + 'px';
+	bubbles.style.top = y + 'px';
+
+	var size = Math.random() * 100;
+	bubbles.style.width = 20 + size + 'px';
+	bubbles.style.height = 20 + size + 'px';
+	ballons.appendChild(bubbles);
+
+	setTimeout(function(){
+		bubbles.remove();
+	},4000)
+})
